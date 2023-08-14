66f0ca2ec06b0a61ccbcf03b26e939eebeb93eeb
Krish120003 <krish120003@gmail.com>
2023-08-14 19:33:00 -0400

    very basic tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,81 @@
+use std::ops::Add;
+
+#[derive(Debug)]
+enum Token {
+    Text(String),
+    Asterisk,
+    Underscore,
+}
+
+#[derive(Debug)]
+struct TokenizedString {
+    source: String,
+    pub tokens: Vec<Token>,
+}
+
+impl TokenizedString {
+    fn new(source: String) -> TokenizedString {
+        TokenizedString {
+            source: source.clone(),
+            tokens: tokenize(source),
+        }
+    }
+}
+
+fn tokenize(source: String) -> Vec<Token> {
+    // print the source
+    println!("source: {:?}", source);
+
+    // create a vector to hold the tokens
+
+    let mut tokens = Vec::new();
+
+    let mut window = String::new();
+
+    let mut escape_next = false;
+    // iterate over the characters in the source
+
+    for char in source.chars() {
+        if escape_next {
+            window.push(char);
+            escape_next = false;
+            continue;
+        }
+
+        match char {
+            '\\' => {
+                escape_next = true;
+            }
+            '*' => {
+                if window.len() > 0 {
+                    tokens.push(Token::Text(window.clone()));
+                    window.clear();
+                }
+                tokens.push(Token::Asterisk);
+            }
+            '_' => {
+                if window.len() > 0 {
+                    tokens.push(Token::Text(window.clone()));
+                    window.clear();
+                }
+                tokens.push(Token::Underscore);
+            }
+            _ => {
+                window.push(char);
+            }
+        }
+    }
+
+    if window.len() > 0 {
+        tokens.push(Token::Text(window.clone()));
+    }
+
+    tokens
+}
+
 fn main() {
-    println!("Hello, world!");
+    let source = String::from("This is some **bold** and _italics_ and ***bold italics*** text.");
+
+    let tokenized_struct = TokenizedString::new(source);
+    println!("{:?}", tokenized_struct.tokens);
 }
