3b51df489449132e8a495da234e90f73ad26bf78
Krish120003 <krish120003@gmail.com>
2023-08-16 14:14:04 -0400

    updates, but also tests are broken???

--- src/main.rs
+++ src/main.rs
@@ -80,64 +80,106 @@ fn tokenize(source: String) -> Vec<Token> {
     tokens
 }
 
-enum AST {
-    Body(Vec<AST>),
-    Bold(Vec<AST>),
-    Italics(Vec<AST>),
+#[derive(Debug, PartialEq, Eq)]
+enum ASTNode {
+    Node(Vec<ASTNode>),
+    Bold(Vec<ASTNode>),
+    Italics(Vec<ASTNode>),
     Text(String),
 }
 
-fn parse(ts: &mut TokenizedString) {
-    let children = Vec::new();
+fn parse(ts: &mut TokenizedString) -> ASTNode {
+    let mut children: Vec<ASTNode> = Vec::new();
 
-    let next = ts.pop();
-    print!("next: {:?}", next);
+    loop {
+        let next = ts.pop();
+        println!("==================");
+        println!("next: {:?}", next);
 
-    return match next {
-        Token::EOF => {
-            AST::Body(children);
+        if next == Token::EOF {
+            break;
         }
-        Token::Asterisk => {
-            // bold
-            if ts.peek(0) == Token::Asterisk {
-                // recursively parse bold text
+
+        match next {
+            Token::Asterisk => {
+                // bold
+                if ts.peek(0) == Token::Asterisk {
+                    println!("FOUND START OF BOLD");
+                    // consume the second asterisk
+                    ts.pop();
+
+                    // now lets find if/where this bold ends, by finding the next double asterisk
+                    let mut offset = 1;
+                    while ts.peek(offset) != Token::Asterisk
+                        && ts.peek(offset + 1) != Token::Asterisk
+                        && ts.peek(offset) != Token::EOF
+                        && ts.peek(offset + 1) != Token::EOF
+                    {
+                        offset += 1;
+                    }
+
+                    // check if EOF
+                    if ts.peek(offset) == Token::EOF || ts.peek(offset + 1) == Token::EOF {
+                        todo!("unmatched asterisk");
+                    }
+
+                    println!("TOKENS RN {:?}", ts);
+
+                    // we must have found the end of the bold text
+                    // lets parse the bold text
+
+                    let mut t = vec![];
+                    for i in 0..offset {
+                        t.push(ts.pop())
+                    }
+
+                    t.push(Token::EOF);
+
+                    let mut bold_text = TokenizedString::new(String::from(""));
+                    bold_text.tokens = t;
+
+                    println!("bold_text: {:?}", bold_text.tokens);
+                    println!("TOKENS RN {:?}", ts);
+
+                    // remove the closing
+                    ts.pop();
+                    ts.pop();
+
+                    // parse the bold text
+                    children.push(parse_bold(&mut bold_text));
+                }
             }
-        }
-        _ => {}
-    };
+            Token::Text(s) => children.push(ASTNode::Text(s)),
+            _ => {}
+        };
+    }
+
+    ASTNode::Node(children)
 }
 
-fn parseBold(ts: &mut TokenizedString) {
-    let children = Vec::new();
-
-    let next = ts.pop();
-    print!("next: {:?}", next);
-
-    return match next {
-        Token::EOF => {
-            AST::Body(children);
-        }
-        Token::Asterisk => {
-            // bold
-            if ts.peek(0) == Token::Asterisk {
-                // recursively parse bold text
-            }
-        }
-        _ => {}
-    };
+fn parse_bold(ts: &mut TokenizedString) -> ASTNode {
+    return ASTNode::Bold(vec![parse(ts)]);
 }
 
 fn main() {
-    // let source = String::from("This is some **bold** and _italics_ and ***bold italics*** text.");
+    let source = String::from("Hello **World**");
+    let mut tokens = TokenizedString::new(source);
+    let ast = parse(&mut tokens);
 
-    // let tokenized_struct = TokenizedString::new(source);
-    // println!("{:?}", tokenized_struct.tokens);
+    let expected_ast = ASTNode::Node(vec![
+        ASTNode::Text(String::from("Hello ")),
+        ASTNode::Bold(vec![ASTNode::Text(String::from("World"))]),
+    ]);
 
-    let src = String::from("a **b**");
-    let mut ts = TokenizedString::new(src);
-    let res = parse(&mut ts);
-    println!("{:?}", res);
+    println!("AST: {:?}", ast);
+    println!("EXPECTED AST: {:?}", expected_ast);
+
+    assert_eq!(1, 1)
 }
+
+// ==============================================
+//                     TESTS
+// ==============================================
 
 #[cfg(test)]
 #[test]
@@ -146,10 +188,13 @@ fn basic_ast() {
     let mut tokens = TokenizedString::new(source);
     let ast = parse(&mut tokens);
 
-    let expected_ast = AST::Body(vec![
-        AST::Text(String::from("Hello ")),
-        AST::Bold(vec![AST::Text(String::from("World"))]),
+    let expected_ast = ASTNode::Node(vec![
+        ASTNode::Text(String::from("Hello ")),
+        ASTNode::Bold(vec![ASTNode::Text(String::from("World"))]),
     ]);
+
+    println!("AST: {:?}", ast);
+    println!("EXPECTED AST: {:?}", expected_ast);
 
     assert_eq!(1, 1)
 }
